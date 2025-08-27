3fbec7dbea7b062d28e6bcd1a568bd814668eea0
IDBOT-dev <DEV@IDBOT.SPACE>
2025-08-27 10:56:43 +0800

    Merge branch 'v2-chat-btc-test' into v2-chat

--- src/views/talk/components/ChannelMemberListDrawer.vue
+++ src/views/talk/components/ChannelMemberListDrawer.vue
@@ -93,7 +93,7 @@ const scrollToTop = () => {
 }
 
 const currentChannelInfo = computed(() => {
-  return talkStore?.activeCommunity?.channels?.find(item => item.pinId === route.params.channelId)
+  return talkStore?.activeCommunity?.channels?.find(item => item.groupId === route.params.channelId)
 })
 
 // 监听currentChannelInfo变化，重新拉取成员数据
